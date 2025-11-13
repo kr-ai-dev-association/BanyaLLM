2b6809f545b578a67f1654dca322986c0f708756
Tony <first@daiblab.com>
2025-11-13 10:42:59 +0900

    Initial Commit

---
+++ BanyaLLM/BanyaLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanyaLLMApp.swift
+//  BanyaLLM
+//
+//  Created by Tony-M4 on 11/13/25.
+//
+
+import SwiftUI
+
+@main
+struct BanyaLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
